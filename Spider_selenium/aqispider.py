db47aae24ca99605eddede7e2a3ba4d7a2e9dbf9
efun <syf199407@163.com>
2019-05-25 19:42:00 +0800

    修改为面向对象爬虫

--- Spider_selenium/aqispider.py
+++ Spider_selenium/aqispider.py
@@ -1,72 +1,93 @@
-from bs4 import BeautifulSoup
 from selenium import webdriver
-import time, csv
+from bs4 import BeautifulSoup
+import time, csv, os
 
 
-def open_url(url, browser):#模拟chrome打开网页#
-    browser.get(url)
-    time.sleep(0.5)#间隔0.5s,表格中没有数据就设置长一点#
-    html = chrome.page_source
-    soup = BeautifulSoup(html, features='lxml')
-
-    return soup
-
-
-def get_month():#爬取月份#
-    monthlist = []
-    for i in range(1, 10):
-        monthlist.append(f'2017-0{i}')
-    for i in range(10, 13):
-        monthlist.append(f'2017-{i}')
-    for i in range(1, 10):
-        monthlist.append(f'2018-0{i}')
-    monthlist.append(f'2018-10')
-
-    return monthlist
-
-
-def get_city():#爬取城市#
-    citylist = []
-    cityurl = 'https://www.aqistudy.cn/historydata/'
-    citysoup = open_url(cityurl, chrome)
-    cityset = citysoup.findAll('div', {'class':'all'})[0].findAll('li')
-    for city in cityset:
-        city = city.text.strip()
-        citylist.append(city)
-
-    return citylist
-
-
-def main():
-    monthlist = get_month()
-    citylist = get_city()
-
-    for city in citylist:
-        #表头#
-        heads = ['date', 'AQI', '质量等级', 'PM2.5', 'PM10', 'SO2', 'CO', 'NO2', 'O3_8h']
-        with open(f'{city}.csv', 'a', newline='') as f:
-            f_csv = csv.writer(f)
-            f_csv.writerow(heads)
-
-        for month in monthlist:
-            url = f'https://www.aqistudy.cn/historydata/daydata.php?city={city}&month={month}'
-            soup = open_url(url, chrome)
-            table = soup.find('table')
-
-            num = str(table).count('<tr')
-            for i in range(num):
-                if i != 0:
-                    tdata = table.find_all('tr')[i].find_all('td')
+class AqiStudy():
+    '''
+    爬取地级市日度空气质量数据并保存为 .csv格式
+    '''
+    
+    
+    # 初始化浏览器
+    def __init__(self, browser):
+        self._browser = browser
+    
+    
+    # 模拟chrome打开网页
+    def open_web(self, url):
+        self._browser.get(url)
+        time.sleep(0.5)
+        html = self._browser.page_source
+        soup = BeautifulSoup(html, 'lxml')
+        return soup
+    
+    
+    # 爬取城市列表
+    def get_city(self):
+        citylist = []
+        city_url = 'https://www.aqistudy.cn/historydata/'
+        city_soup = self.open_web(city_url).find('div', class_='all').find_all('li')
+        for li in city_soup:
+            city = li.a.string.strip()
+            citylist.append(city)
+        return citylist
+    
+    
+    # 爬取月份列表
+    def get_month(self):
+        monthlist = []
+        month_url = 'https://www.aqistudy.cn/historydata/monthdata.php?city=%E5%8C%97%E4%BA%AC'
+        month_soup = self.open_web(month_url).find('ul', class_='unstyled1').find_all('li')
+        for li in month_soup:
+            month_str = li.a.string.strip()
+            month = f'{month_str[:4]}-{month_str[5:-1]}'
+            monthlist.append(month)
+        return monthlist
+    
+    
+    # 创建保存图片的文件夹
+    def create_folder(self, cwd):
+        os.chdir(cwd)
+        if not os.path.exists('AQI'):
+            os.mkdir('AQI')
+        os.chdir('AQI')
+    
+    
+    def main(self):
+        print('爬虫程序启动')
+        monthlist = self.get_month()
+        citylist = self.get_city()
+        current_cwd = os.getcwd()
+        self.create_folder(current_cwd)
+        
+        for city in citylist:
+            #设置表头#
+            heads = ['date', 'AQI', '质量等级', 'PM2.5', 'PM10', 'SO2', 'CO', 'NO2', 'O3_8h']
+            with open(f'{city}.csv', 'w', newline='') as f:
+                f_csv = csv.writer(f)
+                f_csv.writerow(heads)
+            print(f'正在爬取{city}的空气质量数据...')
+            
+            for month in monthlist:
+                url = f'https://www.aqistudy.cn/historydata/daydata.php?city={city}&month={month}'
+                soup = self.open_web(url)
+                table = soup.find('table').find_all('tr')[1:]
+                for tr in table:
+                    tds = tr.find_all('td')
                     data = []
-                    for i in tdata:
-                        data.append(i.text)
+                    for i in tds:
+                        data.append(i.string)
                     with open(f'{city}.csv', 'a', newline='') as f:
                         f_csv = csv.writer(f)
                         f_csv.writerow(data)
-
-    chrome.quit()
+            print(f'{city}数据爬取完毕')
+        print('全部数据爬取完毕')
+        
 
 
 if __name__ == '__main__':
     chrome = webdriver.Chrome()
-    main()
+    aqi = AqiStudy(browser=chrome)
+    aqi.main()
+    chrome.quit()
